29d9784fc52ab800f52b6d595ac6dac4edf80870
barros-carlos <barroscarloshl@gmail.com>
2023-05-08 08:47:40 -0300

    Criação dos métodos e testes do métodos criados

--- methods.py
+++ methods.py
@@ -5,3 +5,15 @@ def area_of_rectangle(width, height):
 def perimeter_of_rectangle(width, height):
     perimeter = 2 * (width + height)
     return perimeter
+
+def sum(a, b):
+    return a+b
+
+def difference(a, b):
+    return a-b
+
+def product(a, b):
+    return a*b
+
+def quotient(a, b):
+    return a/b
--- test_methods.py
+++ test_methods.py
@@ -21,3 +21,47 @@ def test_perimeter():
     
     # then the perimeter should be 14
     assert output == 14
+
+def test_sum():
+    # given a number a equals 3 and a number b equals -1
+    a = 3
+    b = -1
+
+    # when we calculate the sum
+    output = methods.sum(a, b)
+    
+    # then the sum should be 2
+    assert output == 2
+
+def test_difference():
+    # given a number a equals 3 and a number b equals -1
+    a = 3
+    b = -1
+
+    # when we calculate the difference
+    output = methods.difference(a, b)
+    
+    # then the difference should be 4
+    assert output == 4
+
+def test_product():
+    # given a number a equals 3 and a number b equals -1
+    a = 3
+    b = -1
+
+    # when we calculate the product
+    output = methods.product(a, b)
+    
+    # then the product should be -3
+    assert output == -3
+
+def test_quotient():
+    # given a number a equals 4 and a number b equals 3
+    a = 4
+    b = 2
+
+    # when we calculate the quotient
+    output = methods.quotient(a, b)
+    
+    # then the division should be 2
+    assert output == 2
